1192214755284d5d621003e63ce79fc854c85693
Green-Hamster <loginoff90@gmail.com>
2024-06-22 02:45:07 +0300

    Unicafe Step 3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,7 +7,11 @@ const Button = (props) => (
 )
 
 const Statistics = (props) => {
-  // ...
+  return (
+    <div>
+      {props.type}: {props.count}
+    </div>
+  )
 }
 
 const Header = (props) => {
@@ -28,10 +32,20 @@ const App = () => {
   const fbHeader = 'give feedback'
   const statHeader = 'statistics'
 
+
   return (
     <div>
       <Header headerValue={fbHeader} />
+      <Button handleClick={()=>setGood(good+1)} text='good' />
+      <Button handleClick={()=>setNeutral(neutral+1)}text='neutral' />
+      <Button handleClick={()=>setBad(bad+1)} text='bad' />
       <Header headerValue={statHeader} />
+      <Statistics type='good' count={good} />
+      <Statistics type='neutral' count={neutral} />
+      <Statistics type='bad' count={bad} />
+      Total: {good+neutral+bad}<br />
+      Average: {(good-bad)/(good+neutral+bad)}<br />
+      Positive: {100/(good+neutral+bad)*good}
     </div>
   )
 }
